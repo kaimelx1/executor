3a19f658737a52f97c546d7632335763c5f9a3b0
kaimelx1 <kaimelx1@gmail.com>
2020-12-24 10:44:29 +0200

    executor v.1.0.1

--- src/test/java/top/desq/executor/MainTest.java
+++ src/test/java/top/desq/executor/MainTest.java
@@ -52,11 +52,11 @@ public class MainTest {
 
     /**
      * Script dependencies structure:
-     *          1 <----\
-     *        /   \     \
-     *       2     3 <--/
-     *      / \   / \  /
-     *     4   5 5   6/
+     *          1 <---\
+     *        /   \    \
+     *       2     3 <-/
+     *      / \   / \ /
+     *     4   5 5   6
      */
     @Test
     public void circularDependenciesTest() {
